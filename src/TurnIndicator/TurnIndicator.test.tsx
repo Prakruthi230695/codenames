dc140355864e4322e36688115f9ba924d47b2f3e
swpease <swpease@gmail.com>
2018-06-07 13:54:32 -0700

    unit tests
    doesn't seem to be much to do for this component, which I suppose is why I picked it first...

--- src/TurnIndicator/TurnIndicator.test.tsx
+++ src/TurnIndicator/TurnIndicator.test.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { createShallow } from '@material-ui/core/test-utils';
+
 import TurnIndicator from './TurnIndicator';
 
 
@@ -28,4 +29,16 @@ const setup = (propOverrides?: PropOverrides) => {
 
 it('renders without crashing', () => {
   setup();
+});
+
+it('displays the team\'s turn', () => {
+  let wrapper = setup({turn: 'blue'});
+  expect(wrapper.text().toLowerCase()).toMatch(/blue.+turn/);
+  wrapper = setup({turn: 'red'});
+  expect(wrapper.text().toLowerCase()).toMatch(/red.+turn/);
+});
+
+it('displays when a team has won', () => {
+  let wrapper = setup({winner: 'red'});
+  expect(wrapper.text().toLowerCase()).toMatch(/w[io]n/);
 });
