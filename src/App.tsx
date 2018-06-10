240ef76cea8341ba0403ffc98956c55e00947591
swpease <swpease@gmail.com>
2018-06-10 11:28:11 -0700

    baseline styling
    incorportaed CssBaseline and createMuiTheme / MuiTHemeProvider for use throughout app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
 import * as React from 'react';
-import './App.css';
 
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+
+import green from '@material-ui/core/colors/green';
+import purple from '@material-ui/core/colors/purple';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import './App.css';
 import logo from './logo.svg';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green
+  }
+});
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
