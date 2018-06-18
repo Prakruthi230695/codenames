cf8b655ef414201f645e575f1590203635cbc1ce
swpease <swpease@gmail.com>
2018-06-18 12:57:49 -0700

    WordTile stylings start
    He's done it!

--- src/WordTile/WordTile.tsx
+++ src/WordTile/WordTile.tsx
@@ -9,7 +9,55 @@ import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 
 
+enum ClassNames {
+  TextGuessedNonNeutral = "textGuessedNonNeutral",
+  TextSpymasterUnguessedRed = "textSpymasterUnguessedRed",
+  TextSpymasterUnguessedBlue = "textSpymasterUnguessedBlue",
+  TextDefault = "textDefault",
+  PaperSpymasterUnguessedDeath = "paperSpymasterUnguessedDeath",
+  PaperGuessedRed = "paperGuessedRed",
+  PaperGuessedBlue = "paperGuessedBlue",
+  PaperGuessedDeath = "paperGuessedDeath",
+  PaperGuessedNeutral = "paperGuessedNeutral",
+  PaperDefault = "paperDefault"
+}
+
 const styles = (theme: Theme) => createStyles({
+  [ClassNames.TextDefault]: {},
+  [ClassNames.TextGuessedNonNeutral]: {
+    color: theme.palette.common.white,
+  },
+  [ClassNames.TextSpymasterUnguessedRed]: {
+    color: theme.palette.primary.main,
+  },
+  [ClassNames.TextSpymasterUnguessedBlue]: {
+    color: theme.palette.secondary.main,
+  },
+
+  [ClassNames.PaperDefault]: {
+    backgroundColor: theme.palette.grey.A100,
+  },
+  [ClassNames.PaperSpymasterUnguessedDeath]: {
+    backgroundColor: theme.palette.grey.A700,
+  },
+  [ClassNames.PaperGuessedRed]: {
+    backgroundColor: theme.palette.primary.dark,
+  },
+  [ClassNames.PaperGuessedBlue]: {
+    backgroundColor: theme.palette.secondary.dark,
+  },
+  [ClassNames.PaperGuessedNeutral]: {
+    backgroundColor: theme.palette.grey.A400,
+  },
+  [ClassNames.PaperGuessedDeath]: {
+    backgroundColor: theme.palette.common.black,
+  },
+
+  paperPositioning: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
 
 interface Props extends WithStyles<typeof styles> {
@@ -35,6 +83,8 @@ class WordTile extends React.Component<Props, State> {
     }
 
     this.handleClick = this.handleClick.bind(this);
+    this.generatePaperColorClass = this.generatePaperColorClass.bind(this);
+    this.generateTextColorClass = this.generateTextColorClass.bind(this);
   }
 
   handleClick(e: any): void {
@@ -44,12 +94,61 @@ class WordTile extends React.Component<Props, State> {
     }
   }
 
+  generatePaperColorClass(): ClassNames {
+    const { groupedWord, playerType } = this.props;
+    const { guessed } = this.state;
+
+    if (guessed) {
+      if (groupedWord.group === "red") {
+        return ClassNames.PaperGuessedRed;
+      } else if (groupedWord.group === "blue") {
+        return ClassNames.PaperGuessedBlue;
+      } else if (groupedWord.group === "neutral") {
+        return ClassNames.PaperGuessedNeutral;
+      } else {
+        return ClassNames.PaperGuessedDeath;
+      }
+    } else if (playerType === "spymaster" && groupedWord.group === "death") {
+      return ClassNames.PaperSpymasterUnguessedDeath;
+    } else {
+      return ClassNames.PaperDefault;
+    }
+  }
+
+  generateTextColorClass(): ClassNames {
+    const { groupedWord, playerType } = this.props;
+    const { guessed } = this.state;
+
+    if (guessed) {
+      if (groupedWord.group !== "neutral") {
+        return ClassNames.TextGuessedNonNeutral;
+      }
+    } else if (playerType === "spymaster") {
+      if (groupedWord.group === "red") {
+        return ClassNames.TextSpymasterUnguessedRed;
+      } else if (groupedWord.group === "blue") {
+        return ClassNames.TextSpymasterUnguessedBlue;
+      }
+    }
+    return ClassNames.TextDefault;
+  }
+
   render() {
+    const { groupedWord, classes } = this.props;
+
+    const paperColorClassKey: ClassNames = this.generatePaperColorClass();
+    const paperColorClass: string = classes[paperColorClassKey];  // as string?
+    const paperPositioningClass: string = classes["paperPositioning"];
+    const paperClasses: string = paperColorClass + " " + paperPositioningClass;
+
+    const textColorClassKey: ClassNames = this.generateTextColorClass();
+    const textColorClass: string = classes[textColorClassKey];
+
     return (
       <GridListTile onClick={this.handleClick}>
-        <Paper>
-          <Typography>
-            {this.props.groupedWord.word}
+        <Paper className={paperClasses}>
+          <Typography className={textColorClass}>
+            {groupedWord.word}
           </Typography>
         </Paper>
       </GridListTile>
